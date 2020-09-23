e90a099fb0731c40cb919de10ae2daf5a90d88bf
Frank Mularcik <63128305+FrankMularcik@users.noreply.github.com>
2020-09-22 20:30:43 -0400

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+from datetime import timedelta
 import gspread
+
+import os
 
 import yahoo_fin.stock_info as si
 from oauth2client.service_account import ServiceAccountCredentials
+
+os.chdir(r"C:\Users\mular\Python")
 
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
@@ -9,23 +15,33 @@ creds =  ServiceAccountCredentials.from_json_keyfile_name('Sheets API key.json',
 
 client = gspread.authorize(creds)
 
-sheet = client.open('Python Test').sheet1
+sheet = client.open('Portfolio').sheet1
 
-for i in range(2, 23):	
+def datePlusOne(str):
+	date = datetime.strptime(str, '%b %d, %Y')
+	addOne = timedelta(days = 1)
+	date = date + addOne
+	strDate = date.strftime('%b %d, %Y')
+	return strDate;
 
+for j in range(2, 30):	
+	if(sheet.cell(j, 1).value == ""):
+		break;
+
+for i in range(2, j):
 	ticker = sheet.cell(i, 1).value
 	data = si.get_stats(ticker)
 
-	ex_date = data.loc[25, 'Value']
-	sheet.update_cell(i, 6, ex_date)
+	ex_date = data['Value'][25]
+	sheet.update_cell(i, 20, datePlusOne(ex_date))
 
-	pay_date = data.loc[24, 'Value']
-	sheet.update_cell(i, 7, pay_date)
+	pay_date = data['Value'][24]
+	sheet.update_cell(i, 21, datePlusOne(pay_date))
 	
-	div = data.loc[18, 'Value']
-	if isinstance(div, float) : sheet.update_cell(i, 4, "None")
-	else : sheet.update_cell(i, 4, div)
+	div = data['Value'][18]
+	if isinstance(div, float) : sheet.update_cell(i, 9, "")
+	else : sheet.update_cell(i, 9, div)
 	
-	beta = data.loc[0, 'Value']
-	if isinstance(beta, float) : sheet.update_cell(i, 8, "No Data")
-	else : sheet.update_cell(i, 8, beta)
+	beta = data['Value'][0]
+	if isinstance(beta, float) : sheet.update_cell(i, 22, "")
+	else : sheet.update_cell(i, 22, beta)
